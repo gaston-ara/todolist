2c9541fa428bb8821c621b4ecb3c070da11e6184
gaston.ara <gaston.ara.o@gmail.com>
2022-03-28 00:35:00 -0300

    Js terminado

--- script.js
+++ script.js
@@ -1,50 +1,80 @@
-// const tareas = [];
-window.onload = function () {
-    validar()
-};
-
+//Variables
+const form = document.getElementById('form');
+const input = document.getElementById('input-text');
+const lista = document.getElementById('contenedor-lista');
 let tareas = [];
 
-//Funcion de onclick para agregar nueva tarea
-function nueva(event) {
-    event.preventDefault();
-    let inputTarea = document.querySelector("#input-text");
-    tareas.push(inputTarea.value);
-    //Se guarda el array de las tareas en localstorage
-    let Storage = JSON.stringify(tareas);
-    localStorage.setItem('tarea', Storage);
-    //Funcion para renderizar cada tarea nueva
-    renderTarea();
-    //funcion para borrar el input
-    resetInput();
-}
-function resetInput() {
-    let inputTarea = document.querySelector("#input-text");
-    return inputTarea.value = '';
-}
-function renderTarea() {
-    const container = document.querySelector('#contenedor-lista');
-    const tareasStorage = JSON.parse(localStorage.getItem('tarea'))
-    container.innerHTML = "";
-    //For-of que toma datos del localstorage e inserta una plantilla por cada objeto guardado.
-    for (const tarea of tareasStorage) {
-        container.innerHTML += `<div class="item">
-    <div class="tarea">
-        <p>${tarea}</p>
-    </div>
-    <div class="buttons">
-        <button id="done">Done</button>
-        <button id="delete">Delete</button>
-    </div>
-</div>`
+//Funciones
+const validar = () => {
+    if (localStorage.getItem('tareas') === null) {
+        tareas = [];
+    } else {
+        tareas = JSON.parse(localStorage.getItem('tareas'));
     }
 }
+const cleanInput = () => {
+    input.value = '';
+    input.focus();
+}
+const hecho = (e) =>{
+    let toDone = e.parentElement.parentElement.id;
+    tareas[toDone].done = !tareas[toDone].done;
+    renderTask();
+    updateTask();
+}
+const eliminarTask = (e) => {
+    let toDelete = e.parentElement.parentElement.id;
+    tareas.splice(toDelete, 1);
+    renderTask();
+    updateTask();
+}
+const updateTask = () =>{
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+}
+const renderTask = () => {
+    lista.innerHTML = "";
+    for (let i = 0; i < tareas.length; i++) {
+        if (tareas[i].done) {
+            lista.insertAdjacentHTML("beforeend", `<div class="item" id="${i}">
+        <div class="tarea done">
+            <p>${tareas[i].name}</p>
+        </div>
+        <div class="buttons">
+            <button id="done" onclick="hecho(this)">Done</button>
+            <button id="delete" onclick="eliminarTask(this)">Delete</button>
+        </div>
+    </div>`)
+        } else {
+            lista.insertAdjacentHTML("beforeend", `<div class="item" id="${i}">
+        <div class="tarea">
+            <p>${tareas[i].name}</p>
+        </div>
+        <div class="buttons">
+            <button id="done" onclick="hecho(this)">Done</button>
+            <button id="delete" onclick="eliminarTask(this)">Delete</button>
+        </div>
+    </div>`)
+        }
+    }
 
-function validar() {
-    try {
-        tareas = JSON.parse(localStorage.getItem('tarea')) || [];
-        renderTarea();
-    }
-    catch (error) {
-    }
 }
+const newTask = () => {
+    let tarea = {
+        name: input.value,
+        done: false
+    }
+    tareas.push(tarea);
+    console.log(tareas);
+}
+window.onload = () => {
+    validar();
+    renderTask()
+};
+//EventListeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    newTask();
+    renderTask();
+    updateTask();
+    cleanInput();
+});
